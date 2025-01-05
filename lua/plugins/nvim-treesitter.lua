33c5d69379fdc6dda9c77d1f43d56204ba921f8c
Ryo Uchiyama <ryo.uchiyama12@gmail.com>
2025-01-05 22:25:17 +0900

    treesitterの導入

---
+++ lua/plugins/nvim-treesitter.lua
@@ -0,0 +1,26 @@
+return {
+    'nvim-treesitter/nvim-treesitter',
+    version = '*',
+    config = function()
+        require('nvim-treesitter').setup {
+            ensure_installed = { 'lua', 'typescript', 'tsx', 'javascript', 'jsdoc', 'html', 'python', 'apex' },
+            sync_install = true,
+            auto_install = true,
+            highlight = {
+                enable = true,
+            },
+            incremental_selection = {
+                enable = true,
+                keymaps = {
+                    init_selection = 'gnn',
+                    node_incremental = 'grn',
+                    scope_incremental = 'grc',
+                    node_decremental = 'grm',
+                },
+            },
+            indent = {
+                enable = true,
+            },
+        }
+    end
+}
